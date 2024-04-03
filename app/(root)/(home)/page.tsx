4db7439b3d5e54b7c56197477f108c1a2eb39403
Iftequar Ahmed <iftequar.154@gmail.com>
2024-04-03 23:01:17 +0530

    Added the homepage tiles

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React from "react";
 
 const Home = () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-IN", {
+    dateStyle: "full",
+  }).format(now);
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-        <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover"></div>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-            <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-                Upcoming Meeting at: 12:30 PM
-            </h2>
-            <div className="flex flex-col gap-2">
-                <h1 className='text-4xl font-extralight lg:text-7xl'>11:30 AM</h1>
-                <p className='text-lg font-medium text-sky-1 lg:text-2xl'>Wednesday, April 3rd, 2024</p>
-            </div>
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extralight lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
         </div>
-    </section>
-  )
-}
+      </div>
 
-export default Home
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import React from 'react'
+import HomeCard from './HomeCard'
+import { useRouter } from 'next/navigation'
+
+const MeetingTypeList = () => {
+
+    const [meetingState, setMeetingState] = useState<"isJoiningMeeting" | "isSchedulingMeeting" | "isInstantMeeting" | undefined>();
+    const router = useRouter()
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+
+        <HomeCard iconLink="/icons/add-meeting.svg" color="bg-orange-1" title="New meeting" description="Start an instant meeting" handleClick={() => setMeetingState("isJoiningMeeting")} />
+        <HomeCard iconLink="/icons/schedule.svg" color="bg-purple-1" title="Schedule meeting" description="Plan your meeting" handleClick={() => setMeetingState("isSchedulingMeeting")} />
+        <HomeCard iconLink="/icons/recordings.svg" color="bg-blue-1" title="View Recordings" description="Checkout your recordings" handleClick={() => router.push("/recordings")} />
+        <HomeCard iconLink="/icons/join-meeting.svg" color="bg-yellow-1" title="Join meeting" description="via invitation link" handleClick={() => setMeetingState("isJoiningMeeting")} />
+
+    </section>
+  )
+}
+
+export default MeetingTypeList
