69fe8292246cd77356c775e7de75a72c5985162a
Iftequar Ahmed <iftequar.154@gmail.com>
2024-04-06 01:02:11 +0530

    Added the dialog box

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,21 +1,50 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface MeetingModalProps {
-    isOpen: boolean,
-    onClose: () => void,
-    title: string,
-    className?: string,
-    buttonText?: string,
-    handleClick?: () => void,
-    image?: string,
-    buttonIcon?: string,
-    children?: ReactNode
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  buttonText?: string;
+  handleClick?: () => void;
+  image?: string;
+  buttonIcon?: string;
+  children?: ReactNode;
 }
 
-const MeetingModal = ({ isOpen, onClose, title, className, children, buttonText, handleClick, image, buttonIcon}: MeetingModalProps) => {
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  children,
+  buttonText,
+  handleClick,
+  image,
+  buttonIcon,
+}: MeetingModalProps) => {
   return (
-    <div>MeetingModal</div>
-  )
-}
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default MeetingModal
+export default MeetingModal;
