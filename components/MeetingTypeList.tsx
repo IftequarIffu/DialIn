5328b0379e0824f971a6fb265a20ccc643d5ecb5
Iftequar Ahmed <iftequar.154@gmail.com>
2024-04-06 01:23:14 +0530

    Finished the Modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -12,13 +12,13 @@ const MeetingTypeList = () => {
     const router = useRouter()
 
     const createMeeting = () => {
-        
+
     }
 
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
 
-        <HomeCard iconLink="/icons/add-meeting.svg" color="bg-orange-1" title="New meeting" description="Start an instant meeting" handleClick={() => setMeetingState("isJoiningMeeting")} />
+        <HomeCard iconLink="/icons/add-meeting.svg" color="bg-orange-1" title="New meeting" description="Start an instant meeting" handleClick={() => setMeetingState("isInstantMeeting")} />
         <HomeCard iconLink="/icons/schedule.svg" color="bg-purple-1" title="Schedule meeting" description="Plan your meeting" handleClick={() => setMeetingState("isSchedulingMeeting")} />
         <HomeCard iconLink="/icons/recordings.svg" color="bg-blue-1" title="View Recordings" description="Checkout your recordings" handleClick={() => router.push("/recordings")} />
         <HomeCard iconLink="/icons/join-meeting.svg" color="bg-yellow-1" title="Join meeting" description="via invitation link" handleClick={() => setMeetingState("isJoiningMeeting")} />
